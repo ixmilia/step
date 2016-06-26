bde3a0b4509893a2e7a5d449218b5e41e44f88cc
Andreas <strauss.mobil@googlemail.com>
2016-06-26 23:20:03 +0200

    Resolve Problem with other DateTimeFormat

--- src/IxMilia.Step/Syntax/StepSyntaxExtensions.cs
+++ src/IxMilia.Step/Syntax/StepSyntaxExtensions.cs
@@ -37,7 +37,7 @@ namespace IxMilia.Step.Syntax
         public static DateTime GetDateTimeValue(this StepSyntax syntax)
         {
             var str = syntax.GetStringValue();
-            return DateTime.ParseExact(str, StepReader.DateTimeFormat, CultureInfo.InvariantCulture);
+            return DateTime.Parse(str, CultureInfo.InvariantCulture);
         }
 
         public static double GetRealVavlue(this StepSyntax syntax)
