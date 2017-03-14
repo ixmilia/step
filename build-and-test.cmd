5a198fc3d84f8bc5ad0cfb37cb500e7a637a4117
Brett V. Forsgren <brett.forsgren@outlook.com>
2017-03-14 15:49:55 -0700

    don't continue script execution on restore errors

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -1,3 +1,4 @@
 set TEST_PROJECT=.\src\IxMilia.Step.Test\IxMilia.Step.Test.csproj
 dotnet restore %TEST_PROJECT%
+if errorlevel 1 exit /b 1
 dotnet test %TEST_PROJECT%
--- build-and-test.sh
+++ build-and-test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 TEST_PROJECT=./src/IxMilia.Step.Test/IxMilia.Step.Test.csproj
 dotnet restore $TEST_PROJECT
