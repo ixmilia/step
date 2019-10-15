39367dd20cd5a574971c59f64a52ac21cead9b0b
Brett V. Forsgren <brett.forsgren@outlook.com>
2019-10-15 13:01:49 -0700

    allow building Debug/Release

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -1,5 +1,38 @@
-set TEST_PROJECT=.\src\IxMilia.Step.Test\IxMilia.Step.Test.csproj
-dotnet restore %TEST_PROJECT%
+@echo off
+setlocal
+
+set thisdir=%~dp0
+set configuration=Debug
+set runtests=true
+
+:parseargs
+if "%1" == "" goto argsdone
+if /i "%1" == "-c" (
+    set configuration=%2
+    shift
+    shift
+    goto parseargs
+)
+if /i "%1" == "-notest" (
+    set runtests=false
+    shift
+    goto parseargs
+)
+
+echo Unsupported argument: %1
+goto error
+
+:argsdone
+
+:: build
+set SOLUTION=%~dp0src\IxMilia.Step.sln
+dotnet restore %SOLUTION%
 if errorlevel 1 exit /b 1
-dotnet test %TEST_PROJECT%
+dotnet build %SOLUTION% -c %configuration%
 if errorlevel 1 exit /b 1
+
+:: test
+if /i "%runtests%" == "true" (
+    dotnet test "%SOLUTION%" -c %configuration% --no-restore --no-build
+    if errorlevel 1 goto error
+)
--- build-and-test.sh
+++ build-and-test.sh
@@ -1,5 +1,33 @@
 #!/bin/sh -e
 
-TEST_PROJECT=./src/IxMilia.Step.Test/IxMilia.Step.Test.csproj
-dotnet restore $TEST_PROJECT
-dotnet test $TEST_PROJECT
+_SCRIPT_DIR="$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"
+
+CONFIGURATION=Debug
+RUNTESTS=true
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --configuration|-c)
+      CONFIGURATION=$2
+      shift
+      ;;
+    --notest)
+      RUNTESTS=false
+      ;;
+    *)
+      echo "Invalid argument: $1"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+# build
+SOLUTION=$_SCRIPT_DIR/src/IxMilia.Step.sln
+dotnet restore $SOLUTION
+dotnet build $SOLUTION -c $CONFIGURATION
+
+# test
+if [ "$RUNTESTS" = "true" ]; then
+    dotnet test $SOLUTION -c $CONFIGURATION --no-restore --no-build
+fi
